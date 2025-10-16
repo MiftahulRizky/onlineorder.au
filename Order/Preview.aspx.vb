29bbf20114742517a4d99fdfcbcc3052bd1e9912
Miftahul Rizky <duokstayl376@gmail.com>
2025-10-16 16:01:46 +0700

    update Order/Preview.aspx.vb

--- Order/Preview.aspx.vb
+++ Order/Preview.aspx.vb
@@ -1,10 +1,13 @@
-﻿Partial Class Order_Preview
+﻿Imports System.Web
+Partial Class Order_Preview
     Inherits Page
 
     Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim request As HttpRequest = HttpContext.Current.Request
+        Dim baseUrl As String = request.Url.Scheme & "://" & request.Url.Authority & request.ApplicationPath.TrimEnd("/"c)
         If Session("printPreview") <> "" Then
             ' Dim thisString As String = String.Format("https://onlineorder.au/file/order/preview/{0}", Session("printPreview"))
-            Dim thisString As String = String.Format("http://10.0.209.168:8888/file/order/preview/{0}", Session("printPreview"))
+            Dim thisString As String = String.Format(baseUrl & "/file/order/preview/{0}", Session("printPreview"))
             embPrint.Attributes.Add("src", thisString)
         End If
     End Sub
